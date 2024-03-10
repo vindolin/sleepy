078f810acee263ad29a5368e0c447dbf1d074cfb
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-03-10 07:14:23 +0100

    🙄

--- sleepy.py
+++ sleepy.py
@@ -7,7 +7,7 @@ import time
 from pynput import keyboard
 
 SLEEP_AFTER_MINUTES = 15  # amount of minutes of inactivity before triggering shutdown
-SLEEP_DELAY_SEC = 5  # check interval
+CHECK_LOOP_INTERVAL = 60  # check interval seconds
 DO_HIBERNATE = False
 DEBUG = True  # print status icons while running
 
@@ -21,6 +21,13 @@ def check_powercfg():
     # get the output of 'powercfg /requests'
     output = subprocess.check_output(['powercfg', '/requests']).decode()
     return not any(string in output for string in KEEP_AWAKE_STRINGS)
+
+
+last_debug_time = time.time()
+
+
+def print_char(char):
+    print(char, end='')
 
 
 def main():
@@ -53,23 +60,23 @@ def main():
 
         if not sleepy:  # reset counter
             counter = 0
-            DEBUG and print('⚙️', end='')
+            DEBUG and print_char('⚙️')
 
         else:  # increment counter
             if counter > 0:
-                DEBUG and print('💤', end='')
+                DEBUG and print_char('💤')
             counter += 1
 
         sys.stdout.flush()
 
         # this much seconds without waking entries
-        if counter == SLEEP_AFTER_MINUTES * 60 / SLEEP_DELAY_SEC:
+        if counter == SLEEP_AFTER_MINUTES * 60 / CHECK_LOOP_INTERVAL:
             # hibernate or shutdown
             flag = 'h' if DO_HIBERNATE else 'd'
-            subprocess.run(f'psshutdown.exe -${flag}', shell=True)
+            subprocess.run(f'psshutdown.exe -{flag}', shell=True)
             counter = 0
 
-        time.sleep(SLEEP_DELAY_SEC)
+        time.sleep(CHECK_LOOP_INTERVAL)
 
 
 main()
